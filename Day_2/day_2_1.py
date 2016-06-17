95c3887a54f029b93544d2aabaaf3f3c39f8107e
Péricles Feltrin <periclesfeltrin@Pericless-MacBook-Pro.local>
2016-06-17 12:24:16 -0300

    Day 2: ok

--- Day_2/day_2_1.py
+++ Day_2/day_2_1.py
@@ -5,10 +5,7 @@ while True:
     except EOFError:
         print ("Error: EOF or empty input!")
         break
-    l = int(l)
-    w = int(w)
-    h = int(h)
-    list = [(l*w), (w*h), (h*l)]
+    list = [(int(l)*int(w)), (int(w)*int(h)), (int(h)*int(l))]
     total += 2*list[0] + 2*list[1] + 2*list[2]
     list.sort()
     total += list[0]
--- Day_2/day_2_2.py
+++ Day_2/day_2_2.py
@@ -5,10 +5,7 @@ while True:
     except EOFError:
         print ("Error: EOF or empty input!")
         break
-    l = int(l)
-    w = int(w)
-    h = int(h)
-    list = [l, w, h]
+    list = [int(l), int(w), int(h)]
     list.sort()
     total += (list[0] * list[1] * list[2]) + list[0] + list[0] + list[1] + list[1]
 print total
